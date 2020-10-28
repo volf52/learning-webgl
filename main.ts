82659a927e5232bd325fdab8d53dd0426e43ed1b
Arslan <rslnkrmt2552@gmail.com>
2020-10-28 12:16:03 +0300

    Init commit

---
+++ main.ts
@@ -0,0 +1,15 @@
+const main = () => {
+
+  const canvas = document.querySelector<HTMLCanvasElement>("#glCanvas")
+  const gl = canvas.getContext("webgl")
+
+  if (gl === null) {
+    alert("Unable to initialize WebGL")
+    return
+  }
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0)
+  gl.clear(gl.COLOR_BUFFER_BIT)
+}
+
+window.onload = main
