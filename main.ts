4f689954118e4d95129eb6764ab8c7c0be4b9a8c
Arslan <rslnkrmt2552@gmail.com>
2020-10-28 12:28:41 +0300

    add bug

--- main.ts
+++ main.ts
@@ -1,15 +1,16 @@
 const main = () => {
-
-  const canvas = document.querySelector<HTMLCanvasElement>("#glCanvas")
-  const gl = canvas.getContext("webgl")
+  let r = 3;
+  r = "2";
+  const canvas = document.querySelector<HTMLCanvasElement>("#glCanvas");
+  const gl = canvas.getContext("webgl");
 
   if (gl === null) {
-    alert("Unable to initialize WebGL")
-    return
+    alert("Unable to initialize WebGL");
+    return;
   }
 
-  gl.clearColor(0.0, 0.0, 0.0, 1.0)
-  gl.clear(gl.COLOR_BUFFER_BIT)
-}
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+};
 
-window.onload = main
+window.onload = main;
