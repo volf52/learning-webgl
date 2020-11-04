98e1a61d5fd87b25842ac81ed0a346427442ebf7
Arslan <rslnkrmt2552@gmail.com>
2020-11-04 16:17:15 +0300

    Draw triangle

--- main.ts
+++ main.ts
@@ -2,13 +2,58 @@ const main = () => {
   const canvas = document.querySelector<HTMLCanvasElement>("#glCanvas");
   const gl = canvas.getContext("webgl");
 
-  if (gl === null) {
+  if (!gl) {
     alert("Unable to initialize WebGL");
     return;
   }
 
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
+  // vertex data
+  const vertexData = [0, 1, 0, 1, -1, 0, -1, -1, 0];
+
+  // create buffer
+  const buffer = gl.createBuffer();
+
+  // load vertex data into buffer
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
+
+  // create vertex shader
+  const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  gl.shaderSource(
+    vertexShader,
+    `
+  attribute vec3 position;
+  void main(){
+    gl_Position = vec4(position, 1); 
+  }
+`
+  );
+  gl.compileShader(vertexShader);
+  // create fragment shader
+  const fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.shaderSource(
+    fragShader,
+    `
+void main(){
+  gl_FragColor = vec4(1, 0, 0, 1);
+}
+`
+  );
+  gl.compileShader(fragShader);
+  // attach shaders to program
+  const program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragShader);
+  gl.linkProgram(program);
+
+  // enable vertex attribs
+  const posLoc = gl.getAttribLocation(program, `position`);
+  gl.enableVertexAttribArray(posLoc);
+  gl.vertexAttribPointer(posLoc, 3, gl.FLOAT, false, 0, 0);
+  gl.useProgram(program);
+
+  // draw
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
 };
 
 window.onload = main;
