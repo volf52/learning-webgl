bee4597c6d02d134ebc852758eda29adb609a196
Arslan <rslnkrmt2552@gmail.com>
2020-12-18 00:59:35 +0300

    Texture shaders

--- src/shaders.ts
+++ src/shaders.ts
@@ -6,20 +6,19 @@ export const vShaderSrc = `
   precision mediump float;
   
   attribute vec3 ${POS};
-  attribute vec3 ${COLOR};
-  //attribute vec2 ${UV};
+  //attribute vec3 ${COLOR};
+  attribute vec2 ${UV};
   
   uniform mat4 ${M_MAT};
   uniform mat4 ${V_MAT};
   uniform mat4 ${P_MAT};
   
-  //varying vec2 ${V_UV};
-  varying vec3 ${V_COLOR};
+  varying vec2 ${V_UV};
+  //varying vec3 ${V_COLOR};
   
   void main() {
-      //${V_UV} = ${UV};
-      //gl_Position = ${M_MAT} * vec4(${POS}, 1);
-      ${V_COLOR} = ${COLOR};
+      ${V_UV} = ${UV};
+      //${V_COLOR} = ${COLOR};
       mat4 mvp = ${P_MAT} * ${V_MAT} * ${M_MAT};
       gl_Position = mvp * vec4(${POS}, 1);
   }
@@ -28,12 +27,12 @@ export const vShaderSrc = `
 export const fShaderSrc = `
   precision mediump float;
   
-  varying vec3 ${V_COLOR};
-  //varying vec2 ${V_UV};
-  //uniform sampler2D textureID;
+  //varying vec3 ${V_COLOR};
+  varying vec2 ${V_UV};
+  uniform sampler2D textureID;
   
   void main() {
-      //gl_FragColor = vec4(textureID, ${V_UV});
-      gl_FragColor = vec4(${V_COLOR}, 1);
+      gl_FragColor = texture2D(textureID, ${V_UV});
+      //gl_FragColor = vec4(${V_COLOR}, 1);
   }
 `;
